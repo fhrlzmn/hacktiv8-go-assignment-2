1204aea1669b2063c77b430e99538024dab41f51
fhrlzmn <fhrlzmn26@gmail.com>
2023-10-11 09:46:05 +0700

    chore: add foreignKey to Items in Order entity

--- domain/entity/order.go
+++ domain/entity/order.go
@@ -6,7 +6,7 @@ import (
 
 type Order struct {
 	Base
-	CustomerName string    `gorm:"not null;type:varchar(255)"  json:"customer_name"`
-	OrderedAt    time.Time `                                   json:"ordered_at"`
-	Items        []Item    `gorm:"constraint:OnUpdate:CASCADE" json:"items"`
+	CustomerName string    `gorm:"not null;type:varchar(255)"                     json:"customer_name"`
+	OrderedAt    time.Time `                                                      json:"ordered_at"`
+	Items        []Item    `gorm:"foreignKey:OrderID;constraint:OnUpdate:CASCADE" json:"items"`
 }
